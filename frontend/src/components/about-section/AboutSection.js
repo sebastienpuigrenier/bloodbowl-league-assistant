df0af69db306d36a577a5b0968552e824930d336
puigrenier <sebastien.puigrenier@gmail.com>
2024-06-25 18:02:57 +0200

    V1.0.1 -  fix - ajout d'un convertisseur svg -> png pour gérer les problèmes de merge-images dans firefox

--- frontend/src/components/about-section/AboutSection.js
+++ frontend/src/components/about-section/AboutSection.js
@@ -9,11 +9,10 @@ const AboutSection = () => {
                 <h2>A propos du Créateur</h2>
                 <p>Créé par : Sébastien Puigrenier</p>
                 <p>pour : Fil Sanguinaire Choletais</p>
-                <p>Version: 1.0.0</p>
+                <p>Version: 1.0.1</p>
                 <h3>Patch Notes</h3>
                 <ul>
-                    <li>Mise en place de la section versus</li>
-                    <li>Ajout des styles et animations</li>
+                    <li>Correctif de la gestion des svg pour firefox</li>
                 </ul>
             </div>
         </section>
--- frontend/src/components/versus-section/versus-section-components/CalqueCouleurEquipe1.js
+++ frontend/src/components/versus-section/versus-section-components/CalqueCouleurEquipe1.js
@@ -5,8 +5,6 @@ const CalqueCouleurEquipe1 = ({ color }) => {
         <svg
             version="1.0"
             xmlns="http://www.w3.org/2000/svg"
-            width="100%"
-            height="100%"
             viewBox="0 0 3000 800"
             preserveAspectRatio="xMidYMid meet"
         >
